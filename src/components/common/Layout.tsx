fe50682826ab3cdd4efd1dea302659fd4e18c0b6
Luraaya <luraaya@outlook.com>
2025-12-20 13:24:54 +0100

    Update UI layout

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -9,8 +9,8 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <AuroraBackground className="min-h-screen">
-      <div className="relative z-10 flex flex-col min-h-screen">
+    <AuroraBackground className="min-h-[100svh]">
+      <div className="relative z-10 flex min-h-[100svh] flex-col">
         <Header />
         <main className="flex-grow">
           {children}
