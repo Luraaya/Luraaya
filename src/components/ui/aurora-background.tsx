97e55755c4de07396356fd38f65af4f005b23dab
Luraaya <luraaya@outlook.com>
2025-12-20 16:12:52 +0100

    Update UI layout

--- src/components/ui/aurora-background.tsx
+++ src/components/ui/aurora-background.tsx
@@ -53,9 +53,9 @@ export const AuroraBackground = ({
      [background-image:var(--white-gradient),var(--aurora)]
      [background-size:300%,_200%]
      [background-position:50%_50%,50%_50%]
-     opacity-50
+     opacity-70
      blur-[10px]
-     filter sm:invert
+     filter
      will-change-transform
      [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]
      [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
@@ -66,15 +66,15 @@ export const AuroraBackground = ({
      after:[background-size:200%,_100%]
      after:[background-attachment:scroll]
      sm:after:[background-attachment:fixed]
-     after:mix-blend-normal sm:after:mix-blend-difference
+     after:mix-blend-normal
      after:content-[""]
      dark:[background-image:var(--dark-gradient),var(--aurora)]
-     dark:invert-0
      after:dark:[background-image:var(--dark-gradient),var(--aurora)]`,
     showRadialGradient &&
       `[mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]`
   )}
 />
+
 
         </div>
 
